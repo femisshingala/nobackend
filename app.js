1d655196adb7b86c43b0745e45dd045aacaf07eb
Femis <femisshingala54@gmail.com>
2023-09-10 11:52:27 +0530

    sd

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 require('dotenv').config();
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // //middleware
 app.use(fileUpload({
